f2d9efb8f071a67909583c243a2baca03f490867
Roberto Pérez <rpc@tokbox.com>
2015-03-16 12:14:21 +0100

    Implement a capturer which sends noise to the session

--- app/src/main/java/com/tokbox/android/demo/learningopentok/BasicAudioDevice.java
+++ app/src/main/java/com/tokbox/android/demo/learningopentok/BasicAudioDevice.java
@@ -1,13 +1,19 @@
 package com.tokbox.android.demo.learningopentok;
 
 import android.content.Context;
+import android.os.Handler;
 
 import com.opentok.android.BaseAudioDevice;
+
+import java.nio.ByteBuffer;
+import java.util.Random;
 
 public class BasicAudioDevice extends BaseAudioDevice {
     private final static int SAMPLING_RATE = 44100;
     private final static int NUM_CHANNELS_CAPTURING = 1;
     private final static int NUM_CHANNELS_RENDERING = 1;
+
+    private Context mContext;
 
     private AudioSettings mCaptureSettings;
     private AudioSettings mRendererSettings;
@@ -15,33 +21,65 @@ public class BasicAudioDevice extends BaseAudioDevice {
     private boolean mCapturerStarted;
     private boolean mRenderStarted;
 
+    private boolean mAudioDriverPaused;
+
+    private ByteBuffer mCapturerBuffer;
+
+    private Handler mCapturerHandler;
+    private long mCapturerIntervalMillis = 1000;
+    private Runnable mCapturer = new Runnable() {
+        @Override
+        public void run() {
+            mCapturerBuffer.rewind();
+
+            Random rand = new Random();
+            rand.nextBytes(mCapturerBuffer.array());
+
+            getAudioBus().writeCaptureData(mCapturerBuffer, SAMPLING_RATE);
+
+            if(mCapturerStarted && !mAudioDriverPaused) {
+                mCapturerHandler.postDelayed(mCapturer, mCapturerIntervalMillis);
+            }
+        }
+    };
+
     public BasicAudioDevice(Context context) {
+        mContext = context;
+
         mCaptureSettings = new AudioSettings(SAMPLING_RATE, NUM_CHANNELS_CAPTURING);
         mRendererSettings = new AudioSettings(SAMPLING_RATE, NUM_CHANNELS_RENDERING);
 
         mCapturerStarted = false;
         mRenderStarted = false;
+
+        mAudioDriverPaused = false;
+
+        mCapturerHandler = new Handler();
     }
 
     @Override
     public boolean initCapturer() {
+        mCapturerBuffer = ByteBuffer.allocateDirect(SAMPLING_RATE * 2); // Each sample has 2 bytes
         return true;
     }
 
     @Override
     public boolean startCapturer() {
         mCapturerStarted = true;
+        mCapturerHandler.postDelayed(mCapturer, mCapturerIntervalMillis);
         return true;
     }
 
     @Override
     public boolean stopCapturer() {
         mCapturerStarted = false;
+        mCapturerHandler.removeCallbacks(mCapturer);
         return true;
     }
 
     @Override
     public boolean destroyCapturer() {
+        mCapturerBuffer = null;
         return true;
     }
 
@@ -89,11 +127,15 @@ public class BasicAudioDevice extends BaseAudioDevice {
 
     @Override
     public void onPause() {
-
+        mAudioDriverPaused = true;
+        mCapturerHandler.removeCallbacks(mCapturer);
     }
 
     @Override
     public void onResume() {
-
+        mAudioDriverPaused = false;
+        if (mCapturerStarted) {
+            mCapturerHandler.postDelayed(mCapturer, mCapturerIntervalMillis);
+        }
     }
 }
