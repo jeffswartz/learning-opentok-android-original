4df9c12bdacbce5f50d9b3b52b1e6014cb03ec51
Roberto Pérez <rpc@tokbox.com>
2015-03-16 16:06:04 +0100

    Add audio render to a uncompressed file

--- app/src/main/java/com/tokbox/android/demo/learningopentok/BasicAudioDevice.java
+++ app/src/main/java/com/tokbox/android/demo/learningopentok/BasicAudioDevice.java
@@ -5,6 +5,10 @@ import android.os.Handler;
 
 import com.opentok.android.BaseAudioDevice;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Random;
 
@@ -19,11 +23,13 @@ public class BasicAudioDevice extends BaseAudioDevice {
     private AudioSettings mRendererSettings;
 
     private boolean mCapturerStarted;
-    private boolean mRenderStarted;
+    private boolean mRendererStarted;
 
     private boolean mAudioDriverPaused;
 
     private ByteBuffer mCapturerBuffer;
+    private ByteBuffer mRendererBuffer;
+    private File mRendererFile;
 
     private Handler mCapturerHandler;
     private long mCapturerIntervalMillis = 1000;
@@ -43,6 +49,30 @@ public class BasicAudioDevice extends BaseAudioDevice {
         }
     };
 
+    private Handler mRendererHandler;
+    private long mRendererIntervalMillis = 1000;
+    private Runnable mRenderer = new Runnable() {
+        @Override
+        public void run() {
+            mRendererBuffer.clear();
+            getAudioBus().readRenderData(mRendererBuffer, SAMPLING_RATE);
+            try {
+                FileOutputStream stream = new FileOutputStream(mRendererFile);
+                stream.write(mRendererBuffer.array());
+                stream.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (mRendererStarted && !mAudioDriverPaused) {
+                mRendererHandler.postDelayed(mRenderer, mRendererIntervalMillis);
+            }
+
+        }
+    };
+
     public BasicAudioDevice(Context context) {
         mContext = context;
 
@@ -50,11 +80,12 @@ public class BasicAudioDevice extends BaseAudioDevice {
         mRendererSettings = new AudioSettings(SAMPLING_RATE, NUM_CHANNELS_RENDERING);
 
         mCapturerStarted = false;
-        mRenderStarted = false;
+        mRendererStarted = false;
 
         mAudioDriverPaused = false;
 
         mCapturerHandler = new Handler();
+        mRendererHandler = new Handler();
     }
 
     @Override
@@ -79,24 +110,26 @@ public class BasicAudioDevice extends BaseAudioDevice {
 
     @Override
     public boolean destroyCapturer() {
-        mCapturerBuffer = null;
         return true;
     }
 
     @Override
     public boolean initRenderer() {
+        mRendererBuffer = ByteBuffer.allocateDirect(SAMPLING_RATE * 2); // Each sample has 2 bytes
+        mRendererFile = new File(mContext.getFilesDir(), "output.raw");
         return true;
     }
 
     @Override
     public boolean startRenderer() {
-        mRenderStarted = true;
+        mRendererStarted = true;
+        mRendererHandler.postDelayed(mRenderer, mRendererIntervalMillis);
         return true;
     }
 
     @Override
     public boolean stopRenderer() {
-        mRenderStarted = false;
+        mRendererStarted = false;
         return true;
     }
 
@@ -129,6 +162,7 @@ public class BasicAudioDevice extends BaseAudioDevice {
     public void onPause() {
         mAudioDriverPaused = true;
         mCapturerHandler.removeCallbacks(mCapturer);
+        mRendererHandler.removeCallbacks(mRenderer);
     }
 
     @Override
@@ -136,6 +170,9 @@ public class BasicAudioDevice extends BaseAudioDevice {
         mAudioDriverPaused = false;
         if (mCapturerStarted) {
             mCapturerHandler.postDelayed(mCapturer, mCapturerIntervalMillis);
+        }
+        if (mRendererStarted) {
+            mRendererHandler.postDelayed(mRenderer, mRendererIntervalMillis);
         }
     }
 }
